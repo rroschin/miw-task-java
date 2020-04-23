fe2c3d48c2ea05da849c68ca7648aee6293c066d
Roman Roshchin <roschin.roman@gmail.com>
2020-04-22 21:12:20 -0700

    initial project structure with start.spring.io

---
+++ src/main/java/xyz/romros/miwtask/MiwTaskApplication.java
@@ -0,0 +1,13 @@
+package xyz.romros.miwtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiwTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiwTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/romros/miwtask/MiwTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.romros.miwtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiwTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
